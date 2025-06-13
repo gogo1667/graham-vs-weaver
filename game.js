6172a36f1f03fc78579860546ed10e7388d1d2f9
Justin Gough <justin.gough@goftsg.com>
2025-06-13 11:07:33 -0500

    first one

---
+++ game.js
@@ -0,0 +1,55 @@
+let graham = document.getElementById("graham");
+let weaver = document.getElementById("weaver");
+
+let grahamX = 0, grahamY = 0;
+let weaverX = 270, weaverY = 270;
+
+function move(direction) {
+  switch(direction) {
+    case 'up':
+      if (grahamY > 0) grahamY -= 30;
+      break;
+    case 'down':
+      if (grahamY < 330) grahamY += 30;
+      break;
+    case 'left':
+      if (grahamX > 0) grahamX -= 30;
+      break;
+    case 'right':
+      if (grahamX < 330) grahamX += 30;
+      break;
+  }
+  updatePositions();
+}
+
+
+function updatePositions() {
+  graham.style.left = grahamX + "px";
+  graham.style.top = grahamY + "px";
+  weaver.style.left = weaverX + "px";
+  weaver.style.top = weaverY + "px";
+}
+
+function autoMoveWeaver() {
+  if (weaverX > grahamX) weaverX -= 30;
+  else if (weaverX < grahamX) weaverX += 30;
+
+  if (weaverY > grahamY) weaverY -= 30;
+  else if (weaverY < grahamY) weaverY += 30;
+
+  updatePositions();
+
+  // Collision detection
+  if (grahamX === weaverX && grahamY === weaverY) {
+    alert("Weaver got you!");
+    resetGame();
+  }
+}
+
+function resetGame() {
+  grahamX = 0; grahamY = 0;
+  weaverX = 270; weaverY = 270;
+  updatePositions();
+}
+
+setInterval(autoMoveWeaver, 500);
